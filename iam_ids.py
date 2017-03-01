0686946be79854db96aee3e97ea379b6ae852a1e
Pankaj Malhotra(:bitgeeky) <devpankaj1994@gmail.com>
2017-03-01 17:43:20 +0530

    Add exception for iam users created by Vault

--- iam_ids.py
+++ iam_ids.py
@@ -1,10 +1,12 @@
 import boto.iam
+import boto.cloudtrail.layer1
 import ConfigParser
 import sys
 import os.path
 import argparse
 import idsConfig
 import idsNotify
+import json
 
 list_user=""
 
@@ -17,6 +19,7 @@ try:
     data = conn.get_all_users()
 except Exception as e:
     idsNotify.send_alert("AWS IDS: \n"+str(e))
+    print str(e)
     sys.exit(1)
 
 user_list=[]
@@ -26,6 +29,23 @@ for user in data['list_users_response']['list_users_result']['users']:
 user_list_file = ConfigParser.ConfigParser()
 user_list_file.read(os.path.expanduser(args.list_user))
 local_user_names = user_list_file.get("verified_users", "unames", raw=True).split(',')
-diff = list(set(user_list) - set(local_user_names))
+
+try:
+    conn = boto.cloudtrail.layer1.CloudTrailConnection(aws_access_key_id = idsConfig.id, aws_secret_access_key = idsConfig.key)
+    data = conn.lookup_events([{'AttributeKey':'EventName', 'AttributeValue':'CreateUser'},])
+except Exception as e:
+    idsNotify.send_alert("AWS IDS: \n"+str(e))
+    print str(e)
+    sys.exit(1)
+
+users_created_by_vault = []
+
+for event in data['Events']:
+    tmp = json.loads(event['CloudTrailEvent'])
+    if event['Username'] == 'VaultAdminUser':
+	users_created_by_vault.append(tmp['responseElements']['user']['userName'])
+
+diff = list(set(user_list) - set(local_user_names + users_created_by_vault))
+
 if len(diff) > 0:
-    idsNotify.send_alert("AWS IDS: \nUnknown user detected: "+diff)
+    idsNotify.send_alert("AWS IDS: \nUnknown user detected: " + str(diff))
